dcb284182c47528cac099a472af50e5a1c6eb22e
juleesatorres <89477074+juleesatorres@users.noreply.github.com>
2022-02-15 13:41:03 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/la-croix.png') {
+      myImage.setAttribute('src','images/la-croix-lime.png');
+    } else {
+      myImage.setAttribute('src','images/la-croix.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Let us drink La Croix, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Let us drink La Croix, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
